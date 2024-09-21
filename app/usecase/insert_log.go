57ad4d5e9138e463deb834a26a21894f15a04f8e
nayuta-ai <nayuta723@gmail.com>
2024-09-22 04:29:55 +0900

    [Server] Implement Log Usecase.

---
+++ app/usecase/insert_log.go
@@ -0,0 +1,39 @@
+package usecase
+
+import (
+	"context"
+	"time"
+
+	"log_service/app/domain"
+)
+
+type InsertLogUseCase struct {
+	logRepository domain.ILogRepository
+}
+
+func NewInsertLogUseCase(logRepository domain.ILogRepository) *InsertLogUseCase {
+	return &InsertLogUseCase{
+		logRepository: logRepository,
+	}
+}
+
+type InsertLogDto struct {
+	LogLevel           string
+	Date               time.Time
+	DestinationService string
+	SourceService      string
+	RequestType        string
+	Content            string
+}
+
+func (u *InsertLogUseCase) InsertLog(ctx context.Context, dto *InsertLogDto) error {
+	log := domain.NewLog(
+		dto.LogLevel,
+		dto.Date,
+		dto.DestinationService,
+		dto.SourceService,
+		dto.RequestType,
+		dto.Content,
+	)
+	return u.logRepository.Save(ctx, log)
+}
