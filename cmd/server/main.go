4214d7f4d751d1f3201d0c7fbbeeef06ab6bc049
nayuta-ai <nayuta723@gmail.com>
2024-11-12 00:21:32 +0900

    [Server] Refactor cmd/main.go.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,73 +1,13 @@
 package main
 
 import (
-	"context"
 	"log"
-	"os/signal"
-	"syscall"
-	"time"
 
-	"log_service/internal/server/infrastructure/mysql/db"
-	"log_service/internal/server/infrastructure/mysql/repository"
-	"log_service/internal/server/infrastructure/rabbitmq"
-	"log_service/internal/server/presentation"
-	"log_service/internal/server/usecase"
+	"log_service/internal/server"
 )
 
 func main() {
-	ctx, stop := signal.NotifyContext(context.Background(),
-		syscall.SIGINT, syscall.SIGTERM)
-	defer stop()
-
-	db, err := db.Connect()
-	if err != nil {
-		log.Fatalf("Failed to connect to database: %v", err)
-	}
-	defer db.Close()
-
-	logRepo := repository.NewLogRepository(db)
-	logUseCase := usecase.NewInsertLogUseCase(logRepo)
-
-	amqpConn, ch, msgs, err := rabbitmq.Connect()
-	if err != nil {
-		log.Fatalf("Failed to connect to RabbitMQ: %v", err)
-	}
-
-	defer amqpConn.Close()
-	defer ch.Close()
-
-	amqpLogHandler := presentation.NewAMQPLogHandler(logUseCase, ch)
-
-	done := make(chan bool)
-	go func() {
-		for d := range msgs {
-			amqpLogHandler.HandleLog(d)
-			if err := d.Ack(false); err != nil {
-				log.Fatalf("Failed to ack message: %v", err)
-			}
-		}
-		done <- true
-	}()
-
-	log.Printf("Waiting for messages. To exit press CTRL^C")
-
-	<-ctx.Done()
-	stop()
-
-	log.Println("received sigint/sigterm, shutting down...")
-	log.Println("press Ctrl^C again to force shutdown")
-
-	if err := ch.Cancel(rabbitmq.QUEUE_NAME, false); err != nil {
-		log.Panic(err)
-	}
-	if err := ch.Close(); err != nil {
-		log.Panic(err)
-	}
-
-	select {
-	case <-done:
-		log.Println("finished processing all jobs")
-	case <-time.After(5 * time.Second):
-		log.Println("timed out waiting for jobs to finish")
+	if err := server.Run(); err != nil {
+		log.Fatalf("Failed to run server: %v", err)
 	}
 }
---
+++ internal/server/infrastructure/di/log.go
@@ -0,0 +1,37 @@
+package di
+
+import (
+	"go.uber.org/dig"
+
+	"log_service/internal/server/domain"
+	"log_service/internal/server/infrastructure/mysql/db"
+	"log_service/internal/server/infrastructure/mysql/repository"
+	"log_service/internal/server/infrastructure/rabbitmq"
+	"log_service/internal/server/presentation"
+	"log_service/internal/server/usecase"
+)
+
+func BuildLogContainer() (*dig.Container, error) {
+	container := dig.New()
+	if err := container.Provide(db.Connect); err != nil {
+		return nil, err
+	}
+
+	if err := container.Provide(repository.NewLogRepository, dig.As(new(domain.ILogRepository))); err != nil {
+		return nil, err
+	}
+
+	if err := container.Provide(usecase.NewInsertLogUseCase, dig.As(new(usecase.IInsertLogUseCase))); err != nil {
+		return nil, err
+	}
+
+	if err := container.Provide(rabbitmq.Connect); err != nil {
+		return nil, err
+	}
+
+	if err := container.Provide(presentation.NewAMQPLogHandler); err != nil {
+		return nil, err
+	}
+
+	return container, nil
+}
